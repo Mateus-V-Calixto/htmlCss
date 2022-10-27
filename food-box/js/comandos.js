54edf88c91e501b332f8c1ae35b3e3c3177811b3
Mateus Vinicius Calixto <mvcalixto11@gmail.com>
2022-10-27 14:35:10 -0300

    escolha

--- food-box/js/comandos.js
+++ food-box/js/comandos.js
@@ -1,2 +1,46 @@
+var prato ;
+var bebida;
+var sobremesa;
+
+function escolherFrango() {
+    document.getElementById("frango").setAttribute('class','select' )
+    document.getElementById("carne").setAttribute('class', 'produto')
+    prato = "Frango"
+}
+
+function escolherCarne() {
+    document.getElementById("carne").setAttribute('class','select' )
+    document.getElementById("frango").setAttribute('class', 'produto')
+    prato = "Carne"
+}
+
+function escolherCoca() {
+    document.getElementById("coca").setAttribute('class','select' )
+    document.getElementById("refresco").setAttribute('class', 'produto')
+    bebida = "Coca"
+}
+
+function escolherRefresco() {
+    document.getElementById("refresco").setAttribute('class','select' )
+    document.getElementById("coca").setAttribute('class', 'produto')
+    bebida=  "Refresco"
+}
+
+function escolherPudim() {
+    document.getElementById("pudim").setAttribute('class','select' )
+    document.getElementById("sorvete").setAttribute('class', 'produto')
+    sobremesa = 'Pudim'
+}
+
+function escolherSorvete() {
+    document.getElementById("sorvete").setAttribute('class','select' )
+    document.getElementById("pudim").setAttribute('class', 'produto')
+    sobremesa = 'Sorvete'
+}
+
+
+function finalizarPedido() {
+    alert(`${prato}${bebida}${sobremesa}`)
+}
 
 
