41c612dfb58cb71cf5a2839661ad05386734ff0e
Mateus Vinicius Calixto <mvcalixto11@gmail.com>
2022-10-27 15:26:14 -0300

    atualizacoes

--- food-box/js/comandos.js
+++ food-box/js/comandos.js
@@ -5,42 +5,71 @@ var sobremesa;
 function escolherFrango() {
     document.getElementById("frango").setAttribute('class','select' )
     document.getElementById("carne").setAttribute('class', 'produto')
-    prato = "Frango"
+    prato = "frango"
 }
 
 function escolherCarne() {
     document.getElementById("carne").setAttribute('class','select' )
     document.getElementById("frango").setAttribute('class', 'produto')
-    prato = "Carne"
+    prato = "carne"
 }
 
 function escolherCoca() {
     document.getElementById("coca").setAttribute('class','select' )
     document.getElementById("refresco").setAttribute('class', 'produto')
-    bebida = "Coca"
+    bebida = "coca"
 }
 
 function escolherRefresco() {
     document.getElementById("refresco").setAttribute('class','select' )
     document.getElementById("coca").setAttribute('class', 'produto')
-    bebida=  "Refresco"
+    bebida=  "refresco"
 }
 
 function escolherPudim() {
     document.getElementById("pudim").setAttribute('class','select' )
     document.getElementById("sorvete").setAttribute('class', 'produto')
-    sobremesa = 'Pudim'
+    sobremesa = 'pudim'
 }
 
 function escolherSorvete() {
     document.getElementById("sorvete").setAttribute('class','select' )
     document.getElementById("pudim").setAttribute('class', 'produto')
-    sobremesa = 'Sorvete'
+    sobremesa = 'sorvete'
 }
 
 
 function finalizarPedido() {
-    alert(`${prato}${bebida}${sobremesa}`)
+    let msg = montarMensagem(prato, bebida, sobremesa)
+    window.open(`https://wa.me/+5521999999999?text=${msg}`)
+}
+
+function montarMensagem(prato, bebida, sobremesa) {
+    if (prato == null) {
+        prato = ''
+    }else{
+        prato = `${prato}`
+    }
+    
+    if (bebida == null) {
+        bebida = ''
+    }else{
+        bebida = `, ${bebida}`
+    }
+
+    if (sobremesa == null) {
+        sobremesa = ''
+    }else{
+        sobremesa = `e ${sobremesa}`
+    }
+
+
+    let mensagem = `Ola Gostaria de pedir um combo de ${prato}${bebida} ${sobremesa}`
+    return mensagem
+
+    
+        
+
 }
 
 
